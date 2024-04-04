b2628db087967282233dfb8fd97666dfc5cd278b
daun <daun.friend@gmail.com>
2024-04-04 12:13:29 +0900

    java도 넣음

---
+++ java/ch7/funcformat.js
@@ -0,0 +1,13 @@
+function showMsg(){
+    console.log('Hello Javascript');
+}
+showMsg();
+showMsg();
+
+function plus(a,b){
+    console.log('두 수의 합: '+ (a+b));
+}
+plus(100,200);
+plus('ABCD','EFGH');
+plus(3.14,1.59);
+
