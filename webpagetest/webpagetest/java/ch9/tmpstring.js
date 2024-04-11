e5bf18918a6f31f983de1a2010781e61942f3b63
daun <daun.friend@gmail.com>
2024-04-11 10:09:13 +0900

    자바1

---
+++ webpagetest/webpagetest/java/ch9/tmpstring.js
@@ -0,0 +1,9 @@
+const n = '이창현';
+const age = 25;
+const height = 178.5;
+
+
+
+console.log("My name is "+n+ '\n' + "My age is " + age)
+
+// console.log('My name is `{name},My age us `{age}, My height is `{height}')
