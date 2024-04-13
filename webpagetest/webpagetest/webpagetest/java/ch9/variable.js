96d3c1a79cb8504701e766aec4431c86ad303edd
daun <daun.friend@gmail.com>
2024-04-13 14:34:09 +0900

    2ro2ro

---
+++ webpagetest/webpagetest/webpagetest/java/ch9/variable.js
@@ -0,0 +1,27 @@
+function myTest(){
+    if(1){
+        var x = 100;
+    }
+    console.log(x);
+}
+myTest();
+
+
+function myTest(){
+    let x;
+    if(1){
+        x = 100;
+    }
+    console.log(x);
+}
+myTest();
+
+
+function myTest(){
+    const y = 100;
+    if(1){
+        
+    }
+    console.log(y);
+}
+myTest();
